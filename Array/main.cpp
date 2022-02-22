969f235ba4bfd457022cca646432d8a40ff3351f
shu-du <1762722391@qq.com>
2022-02-22 21:44:08 +0800

    2022.02.22

--- Array/main.cpp
+++ Array/main.cpp
@@ -1,3 +1,17 @@
+/* 
+485. 最大连续 1 的个数
+给定一个二进制数组 nums ， 计算其中最大连续 1 的个数。
+示例 1：
+输入：nums = [1,1,0,1,1,1]  输出：3
+
+示例 2:
+输入：nums = [1,0,1,1,0,1]  输出：2
+?
+
+提示：
+1 <= nums.length <= 105
+nums[i]?不是 0 就是 1. */
+
 #include <iostream>
 #include <vector>
 using namespace std;
